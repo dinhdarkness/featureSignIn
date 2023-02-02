7e06e94b63442a6fee1812b38ce483f472118348
dinhdarkness <122354256+dinhdarkness@users.noreply.github.com>
2023-02-02 11:23:43 +0700

    build v1.1

--- build/web-mobile/assets/main/index.f10d6.js
+++ build/web-mobile/assets/main/index.f10d6.js
@@ -176,12 +176,20 @@ window.__require = function e(t, n, r) {
       LoginScreen.prototype.start = function() {
         this.nodeLayoutLogin.active = true;
         this.nodeLayoutSignIn.active = false;
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "https://connect.facebook.net/en_US/sdk.js";
+          fjs.parentNode.insertBefore(js, fjs);
+        })(document, "script", "facebook-jssdk");
         window.fbAsyncInit = function() {
           FB.init({
             appId: "577254864321632",
             cookie: true,
             xfbml: true,
-            version: "v3.3"
+            version: "v15.0"
           });
         };
       };
